e6c60485e08bd37ffde01a6484f2101ca0f2d569
Minesh <hst720077742@gmail.com>
2026-04-28 10:58:29 -0400

    Add Dotenv and MySQL dependencies with environment mapping

--- src/main/java/com/coinbank2/coinbank2/Coinbank2Application.java
+++ src/main/java/com/coinbank2/coinbank2/Coinbank2Application.java
@@ -1,13 +1,21 @@
 package com.coinbank2.coinbank2;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv; // Add this line
 
 @SpringBootApplication
 public class Coinbank2Application {
 
     public static void main(String[] args) {
+        // Load .env variables into System Properties
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(Coinbank2Application.class, args);
     }
+
+
 
 }
