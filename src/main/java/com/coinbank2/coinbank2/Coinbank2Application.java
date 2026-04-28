8311ed8b959c10ffa66a45e4f3db57fe96402964
Minesh <hst720077742@gmail.com>
2026-04-28 09:47:43 -0400

    Initial commit: project structure and database config

---
+++ src/main/java/com/coinbank2/coinbank2/Coinbank2Application.java
@@ -0,0 +1,13 @@
+package com.coinbank2.coinbank2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coinbank2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coinbank2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/coinbank2/coinbank2/Coinbank2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coinbank2.coinbank2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coinbank2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
